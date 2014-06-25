2dd5d015c0634588afcefaf790b43f8495582bf8
greg Sochanik <greg@sochanik.com>
2014-06-25 17:23:54 +0100

    Moved files into 7d buynow project

--- src/SevenDigital.ApiSupportLayer.ServiceStack/Services/Shops/ShopRestriction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SevenDigital.ApiSupportLayer.ServiceStack.Services.Shops
-{
-	public class ShopRestriction
-	{
-		public string IpAddress { get; set; }
-		public string CountryCode { get; set; }
-		public bool IsRestricted { get; set; }
-	}
-}
